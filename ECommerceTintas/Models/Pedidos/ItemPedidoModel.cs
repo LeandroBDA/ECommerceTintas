57642babbe7751efb8bf98443a4436eca20688b9
Isaac <isaacalvesrocha@gmail.com>
2025-02-08 19:40:39 -0300

    PedidosAtualizado

--- ECommerceTintas/Models/Pedidos/ItemPedidoModel.cs
+++ ECommerceTintas/Models/Pedidos/ItemPedidoModel.cs
@@ -6,28 +6,13 @@ namespace ECommerceTintas.Models.Pedidos
 {
     public class ItemPedidoModel
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
         public int PedidoId { get; set; } 
-
-        [ForeignKey("PedidoId")]
         public PedidoModel Pedido { get; set; } = null!;
-
-        [Required]
         public int ProdutoId { get; set; }
-        
-        [ForeignKey("ProdutoId")]
         public ProdutoModel Produto { get; set; } = null!;
-
-        [Required]
         public int Quantidade { get; set; }
-
-        [Required]
         public decimal PrecoUnitario { get; set; }
-        
-        [Required]
         public decimal Subtotal => Quantidade * PrecoUnitario;
     }
 }
--- ECommerceTintas/Models/Pedidos/PedidoModel.cs
+++ ECommerceTintas/Models/Pedidos/PedidoModel.cs
@@ -1,30 +1,16 @@
 using ECommerceTintas.Models.Usuario;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using ECommerceTintas.Models.Enum;
 
 namespace ECommerceTintas.Models.Pedidos
 {
     public class PedidoModel
     {
-        [Key]
         public int Id { get; set; } 
-        
-        [Required]
         public int UsuarioId { get; set; } 
-        
-        [ForeignKey("UsuarioId")]
         public UsuarioModel Usuario { get; set; } = null!; 
-        
-        [Required]
         public DateTime DataPedido { get; set; } = DateTime.Now; 
-        
-        [Required]
         public decimal ValorTotal { get; set; } 
-        
-        [Required]
         public EStatusPedido Status { get; set; } 
-        
         public List<ItemPedidoModel> Itens { get; set; } = new(); 
     }
 }
